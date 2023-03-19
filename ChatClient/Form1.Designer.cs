273727631e9d015b129375ffe828af86842cf4ca
user <user@LAPTOP-58QID8TO>
2023-03-19 22:49:17 +0900

    초기 커밋.

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sendbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(11, 10);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(682, 292);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 325);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // sendbtn
+            // 
+            this.sendbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendbtn.Location = new System.Drawing.Point(585, 318);
+            this.sendbtn.Name = "sendbtn";
+            this.sendbtn.Size = new System.Drawing.Size(108, 38);
+            this.sendbtn.TabIndex = 2;
+            this.sendbtn.Text = "Send";
+            this.sendbtn.UseVisualStyleBackColor = true;
+            this.sendbtn.Click += new System.EventHandler(this.sendbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.sendbtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 358);
+            this.Controls.Add(this.sendbtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sendbtn;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets; // 통신을 하기 위한 첫번째 단계
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient _client;
+        byte[] _buffer = new byte[4096];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _client = new TcpClient();                     
+              
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            _client.Connect("127.0.0.1", 54000); // 포트 번호 임의 생성
+
+
+            _client.GetStream().BeginRead(_buffer,
+                                            0,
+                                            _buffer.Length,
+                                            Server_MessageReceived,                                          null);
+        }
+
+        private void Server_MessageReceived(IAsyncResult ar)
+        {
+            if (ar.IsCompleted)
+            {
+                // TODO : RECEIVE MESSAGE
+                var bytesIn = _client.GetStream().EndRead(ar);
+
+                if (bytesIn > 0)
+                {
+                    var tmp = new byte[bytesIn];
+                    Array.Copy(_buffer, 0, tmp, 0, bytesIn); // 시작위치 0, 바이트가 될 길이가 
+                    var str = Encoding.ASCII.GetString(tmp);
+                    // TODO : MAKE THIS AN ACTUAL DELEGATE READ DOCS
+                    BeginInvoke((Action)(() => // Action F12로 확인해보면 public delegate 공개대리자
+                    {
+                        listBox1.Items.Add(str);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    }));
+                }
+                /*BeginInvoke()*/ // c# 내부 라이브러리. 이작업이 메세지를 받는데 유용하다
+                                  // 쓰레드의 위험성에서 조금 벗어나게 해주는 존재?
+                Array.Clear(_buffer, 0, _buffer.Length);
+
+            }
+        }
+
+        private void sendbtn_Click(object sender, EventArgs e)
+        {
+            var msg = Encoding.ASCII.GetBytes(textBox1.Text);
+            _client.GetStream().Write(msg, 0, msg.Length);
+
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
